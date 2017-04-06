c0133c58d67439241c14f1ffc8f817fa87f835a2
indigo-sam <sam.jin@indigoslate.com>
2017-04-06 00:36:14 -0700

    update

--- app.js
+++ app.js
@@ -27,20 +27,43 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
+
+
+/*
+ * buffer doesn't need require since it's foundation of node, like regular data type in js. 
+ */
+const buf = new Buffer('hello', 'utf8') //'utf8 is default event it's not specified. 'hello' is maximum letters allowed in this buffer when set. 
+
+console.log(buf) //68 65 6c 6c 6f -> this is one type of binary data in hexadecimal notation format. 
+console.log(buf.toString()) //hello
+console.log(buf.toJSON()); // { type: 'Buffer', data: [ 104, 101, 108, 108, 111 ] }   -> character set.
+console.log(buf[0]); // 104
+// write letter to buffer, but since buf only allow 5 letters, it will replace 'hel'
+// buf.write('sam'); // samlo
+// console.log(buf.toString());
+
+const fs = require('fs')
+
+const readfile1 = fs.readFileSync(__dirname + '/readfile', 'utf8')
+console.log(readfile1, 1);
+
+const readfile2 = fs.readFile(__dirname + '/readfile', 'utf8', function(err, data) {
+    console.log(data.toString(), 2); // Most callbacks use err as first parameter. If no err, 'null' wil be return;
+})
---
+++ main.js
@@ -0,0 +1,22 @@
+// const util = require('util')
+// const EventEmitter = require('events')
+
+// class Greeter extends EventEmitter {
+//     constructor() {
+//         super()
+//         this.greeting = 'hello world'
+//     }
+
+//     greet(data) {
+//         console.log(`${this.greeting}:${data}`);
+//         this.emit('greet', data)
+//     }
+// }
+
+// const greeter1 = new Greeter()
+
+// greeter1.on('greet', (data) => console.log(`someone greeted ${data}`))
+
+// greeter1.greet('Tony')
+
+console.log('main.js string file')
